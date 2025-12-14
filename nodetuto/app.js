86577fae69cd306511e67d490bed1674a6078d39
Norien-euh <norah2006norah@outlook.com>
2025-12-14 17:10:04 +0100

    cambio un comentario

--- nodetuto/app.js
+++ nodetuto/app.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Almacenamiento en memoria (me daba un poco de pereza hacer una bd para esto)
+// Almacenamiento en memoria (me daba un poco de pereza hacer una bd para esto y como no se especificaba que hubiera que hacerlo con una bd)
 let nombres = []
 let currentId = 1
 
